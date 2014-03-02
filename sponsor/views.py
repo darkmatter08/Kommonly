80d80d625956f66a04a3a71722b24b1ce2873eb6
amyyin <amyin@college.harvard.edu>
2014-03-02 15:58:27 -0500

    working on email

--- sponsor/views.py
+++ sponsor/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, render_to_response, get_object_or_404, get_
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from sponsor.models import *
+from organizer.models import *
+from django.core.mail import send_mail
+from django import forms
 
 def show_business_dashboard(request):
 	print "Hello there. This is the organizer page"
@@ -21,14 +24,28 @@ def business_profile(request, business_id):
 		funding_types = Sponsor_types_organization.objects.filter(organization_id=business_id)
 		sponsorship_types = []
 		for type in funding_types:
-			
 			sponsorship_types.append(Sponsor_types.objects.get(id=type.funding_type_id))
 	except Organization.DoesNotExist:
 		raise Http404
-	return render(request, 'sponsor/profile.html', {'business':business, 'funding_types':sponsorship_types})
-	
+	name = request.user.first_name if request.user.is_authenticated() else "<your name>"
+	data = {'subject': business.name + " sponsorship for event", 'message':"Dear " + business.contact_fname,sender:request.user.email}
+	return render(request, 'sponsor/profile.html', {'business':business, 'funding_types':sponsorship_types, 'organizer':request.user, 'form':ContactForm(data)})
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100, initial="Sponsorship for ")
+    message = forms.CharField()
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
+
 def contact(request, business_id):
-	if request.user.is_authenticated():
-		return HttpResponse("user exists")
-	else:
-		return HttpResponse("Login required to contact businesses")
+	if not request.user.is_authenticated():
+		return HttpResponse("Please login to contact businesses")
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			send_mail(request.POST['subject'], request.POST['message'], request.POST['organizer_email'],
+    ['amymichelleyin@gmail.com'], fail_silently=False)
+			return HttpResponse('thanks')
+	
+	business = Sponsor.objects.get(id=business_id)
+	return HttpResponse('thanks')
