aa7a98706d29e61e6faf5259d39b92a1de4db0dc
amyyin <amyin@college.harvard.edu>
2014-03-02 11:48:46 -0500

    added business logos to /businesses

--- Kommonly/urls.py
+++ Kommonly/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,7 +11,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'Kommonly.views.home'),
     (r'^/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-
+    (r'^/assets/(?P<path>.*)$', 'django.views.static.serve',
+                 {'document_root': settings.MEDIA_ROOT}),
 )
 
 urlpatterns += patterns('organizer.views',
