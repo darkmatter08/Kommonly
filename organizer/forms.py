fc4c6ad83723bd9bb9ba222f845bc5b8855b188f
Shawn Jain <shawnjain.08@gmail.com>
2014-04-09 02:14:38 -0400

    small login refactor

--- organizer/forms.py
+++ organizer/forms.py
@@ -16,14 +16,14 @@ from django.contrib.auth.models import User
 # 		fields = ['name_user', 'email', 'password', 'organization']
 
 class UserSignUpForm(forms.ModelForm):
-	fname = forms.CharField(max_length = 30, label ="", widget = forms.TextInput(attrs = {'placeholder' : 'First Name', 'style' : 'display: block;'}))
-	lname = forms.CharField(max_length = 30, label ="", widget = forms.TextInput(attrs = {'placeholder' : 'Last Name', 'style' : 'display: block;'}))
+	first_name = forms.CharField(max_length = 30, label ="", widget = forms.TextInput(attrs = {'placeholder' : 'First Name', 'style' : 'display: block;'}))
+	last_name = forms.CharField(max_length = 30, label ="", widget = forms.TextInput(attrs = {'placeholder' : 'Last Name', 'style' : 'display: block;'}))
 	password = forms.CharField(widget=forms.PasswordInput(attrs = {'placeholder' : 'Password', 'style' : 'display: block;'}), label ="",)
 	email = forms.EmailField(max_length = 30,label ="", widget = forms.TextInput(attrs = {'placeholder' : 'Email', 'style' : 'display: block;'}))
 	organization = forms.CharField(max_length = 30, label ="", widget = forms.TextInput(attrs = {'placeholder' : 'Organization', 'style' : 'display: block;'}))
 	class Meta:
 		model = User
-		fields = ['fname', 'lname', 'password', 'email', 'organization' ]    	
+		fields = ['first_name', 'last_name', 'password', 'email', 'organization' ]    	
 
 class UserLoginForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput(attrs = {'placeholder' : 'Password', 'style' : 'display: block;'}), label="")
--- organizer/views.py
+++ organizer/views.py
@@ -15,79 +15,57 @@ from django.contrib.auth.decorators import login_required
 
 
 def organizer_signup(request):
-    if request.method != 'POST':
-        return HttpResponseBadRequest()
-    
-    organizerForm = UserSignUpForm(request.POST)
-    if organizerForm.is_valid():
-        fname = organizerForm.cleaned_data['fname']
-        lname = organizerForm.cleaned_data['lname']
-        password  = organizerForm.cleaned_data['password']
-        email = organizerForm.cleaned_data['email']
-        organization = organizerForm.cleaned_data['organization']
-        user = User.objects.create_user(first_name=fname, last_name=lname, username=email, email=email, password=password)
-        user.backend='django.contrib.auth.backends.ModelBackend' 
-        Organizer.objects.create(user=user, organization=organization)
-        # Check their validity
-        user = authenticate(username=email, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                # Redirect to a success page.
-                return HttpResponseRedirect('/organizer/home')
-            # TODO: add in an error page - try logging in again
-            else:
-                return HttpResponseRedirect('/')
-        else:
-            #TODO: Return an 'invalid login' error message.
-            return HttpResponseRedirect('/')
-
-    return HttpResponseRedirect('/')
-
+	if request.method != 'POST':
+		return HttpResponseBadRequest()
+	
+	organizerForm = UserSignUpForm(request.POST)
+	if organizerForm.is_valid():
+		fname = organizerForm.cleaned_data['first_name']
+		lname = organizerForm.cleaned_data['last_name']
+		password  = organizerForm.cleaned_data['password']
+		email = organizerForm.cleaned_data['email']
+		organization = organizerForm.cleaned_data['organization']
+		user = User.objects.create_user(first_name=fname, last_name=lname, username=email, email=email, password=password)
+		user.backend='django.contrib.auth.backends.ModelBackend' 
+		Organizer.objects.create(user=user, organization=organization)
+		
+		# Log them in and redirect after creating their user object
+		return login_helper(request, organizerForm.cleaned_data['email'], organizerForm.cleaned_data['password'])
+	return HttpResponseRedirect('/')
 
 def organizer_login(request):
-    if request.method != 'POST':
-        return HttpResponseBadRequest()
-    
-    organizerForm = UserLoginForm(request.POST)
-    if organizerForm.is_valid():
+	if request.method != 'POST':
+		return HttpResponseBadRequest()
+	
+	organizerForm = UserLoginForm(request.POST)
+	if organizerForm.is_valid():
+		# Log them in and redirect
+		return login_helper(request, organizerForm.cleaned_data['email'], organizerForm.cleaned_data['password'])
+	else:
+		return HttpResponseRedirect('/')
 
-        username = request.POST['email']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                # Redirect to a success page.
-                return HttpResponseRedirect('/organizer/home')
-            else:
-                # TODO: add in an error page - try logging in again
-                return HttpResponseRedirect('/')
-        else:
-            #TODO: Return an 'invalid login' error message.
-            return HttpResponseRedirect('/')
-    else:
-        return HttpResponseRedirect('/')
-    
+# Method that handles login via Django auth system
+def login_helper(request, username, password):
+	user = authenticate(username=username, password=password)
+	if user is not None:
+		if user.is_active:
+			login(request, user)
+			# Redirect to a success page.
+			return HttpResponseRedirect('/organizer/home')
+		else:
+			# TODO: add in an error page - try logging in again
+			return HttpResponseRedirect('/')
+	else:
+		#TODO: Return an 'invalid login' error message.
+		return HttpResponseRedirect('/')
+	
 @login_required
 def organizer_home(request):
-    currentOrganizer = Organizer.objects.get(user=request.user)
-    events = Event.objects.filter(organizer=currentOrganizer)
-    context = { 'request': request, "organizer": currentOrganizer, "events": events}
-    return render(request, 'organizer/organizer_dashboard.html', context)
-
-# AJAX method to serialize all current events and return as JSON. 
-# Frontend can call this to load all current events. 
-@csrf_exempt
-def getAllEvents(request):
-    if request.method != 'POST':
-        print "bad request"
-        return HttpResponseBadRequest()
-    allEvents = []
-    for currentEvent in Event.objects.all():
-        allEvents.append({"event_date" : currentEvent.event_date.strftime('%Y-%m-%dT%H:%M:%S'), "name" : currentEvent.name, "description" : currentEvent.description})
-    return HttpResponse(json.dumps(allEvents), content_type="application/json")
+	currentOrganizer = Organizer.objects.get(user=request.user)
+	events = Event.objects.filter(organizer=currentOrganizer)
+	context = { 'request': request, "organizer": currentOrganizer, "events": events}
+	return render(request, 'organizer/organizer_dashboard.html', context)
 
 def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect('/')
+	auth.logout(request)
+	return HttpResponseRedirect('/')
